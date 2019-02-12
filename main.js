2be600712ab631b8fa5ee85abbbbd7898bd70551
Hapachino <erickwbrownfield@gmail.com>
2019-02-11 18:16:24 -0800

    experimental branch using async/await for self testing

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ $(document).ready(init);
 
 const MAX_DISPLAY_LENGTH = 10;
 const FRACTION = 6;
+const TEST_INTERVAL = 500;
+const WAIT_INTERVAL = 5000;
 
 const states = {
   equal(input) {
@@ -193,8 +195,10 @@ function updateDisplay() {
   $('.display-container').text(output);
 }
 
+// Self-testing Functions
+
 function SimulateKeyPress(input, duration, activeDuration = 250) {
-  const button = $(`[data-key=${input}]`);
+  const button = $(`[data-key='${input}']`);
 
   button.addClass('hover');
   setTimeout(() => {
@@ -208,8 +212,45 @@ function SimulateKeyPress(input, duration, activeDuration = 250) {
   }, duration - activeDuration);
 }
 
-function selfTest(testCase) {
-  
+function wait(duration) {
+  return new Promise(resolve => setTimeout(resolve, duration));
+} 
+
+function runTestCase(testCase) {
+  const { name, input, output } = testCase;
+
+  $('.test.name').text(name);
+  $('.test.input').text(input);
+  $('.test.output').text(`Expected Output: ${output}`);
+
+  (async () => {
+    for (let key of input) {
+      SimulateKeyPress(key, TEST_INTERVAL);
+
+      await wait(TEST_INTERVAL);
+
+      updateModel(key);
+    }
+
+    $('.test.input').append(model.accumulator);
+
+    return new Promise(resolve => resolve(model.accumulator === output));
+  })();
+}
+
+function runSelfTest(testCases) {
+  (async () => {
+    for (let testCase of testCases) {
+      const match = await runTestCase(testCase);
+      const result = match ? 'pass' : 'fail';
+
+      $('.test.result').text(result);
+
+      if (!match) break;
+
+      wait(WAIT_INTERVAL);
+    }
+  })();
 }
 
 // validates output and displays
--- test_cases.js
+++ test_cases.js
@@ -2,62 +2,62 @@ const testCases = [
   {
     name: 'Addition',
     input: '1+2=',
-    output: '3',
+    output: 3,
   },
   {
     name: 'Multiplication',
     input: '1×2=',
-    output: '2',
+    output: 2,
   },
   {
     name: 'Division',
     input: '1÷2=',
-    output: '0.5',
+    output: 0.5,
   },
   {
     name: 'Subtraction',
     input: '1-2=',
-    output: '-1',
+    output: -1,
   },
   {
     name: 'Successive Operation',
     input: '1+1+2=',
-    output: '4',
+    output: 4,
   },
   {
     name: 'Decimal',
     input: '1.1=',
-    output: '1.1',
+    output: 1.1,
   },
   {
     name: 'Multiple Decimals',
     input: '1...1=',
-    output: '1.1',
+    output: 1.1,
   },
   {
     name: 'Multiple Operation',
     input: '1++++2=',
-    output: '3',
+    output: 3,
   },
   {
     name: 'Changing Operation',
     input: '1+-×/2=',
-    output: '0.5',
+    output: 0.5,
   },
   {
     name: 'Repeat Operation',
     input: '1+1===',
-    output: '4',
+    output: 4,
   },
   {
     name: 'Rollover Operation',
     input: '1+1+=+=',
-    output: '8',
+    output: 8,
   },
   {
     name: 'Successive Operation',
     input: '1+3÷4+10×2=',
-    output: '22',
+    output: 22,
   },
   {
     name: 'Division by Zero',
@@ -67,21 +67,21 @@ const testCases = [
   {
     name: 'Premature Operation',
     input: '++++1×2=',
-    output: '2',
+    output: 2,
   },
   {
     name: 'Partial Operand',
     input: '2×=',
-    output: '4',
+    output: 4,
   },
   {
     name: 'Missing Operation',
     input: '1=',
-    output: '1',
+    output: 1,
   },
   {
     name: 'Missing Operands',
     input: '====',
-    output: '0',
+    output: 0,
   },
 ];
